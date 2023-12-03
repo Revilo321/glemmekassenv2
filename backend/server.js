2288600a07976a54e6fb11071d452ad5dbe7ed50
Revilo321 <oliverwii@live.dk>
2023-12-03 15:59:05 +0100

    bit of cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,96 +1,75 @@
-const express = require("express");
-const http = require('http');
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+const express = require('express')
+const http = require('http')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
+
+const db = require('./app/models')
+db.sequelize.sync()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+require('./app/routes/user.routes')(app)
+require('./app/routes/messages.routes')(app)
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to code squad' })
+})
 
 
-const db = require("./app/models");
-db.sequelize.sync();
-
-
-const users = {};
-
+//Socket for realtime chat
+const users = {}
 io.on('connection', (socket) => {
-
-  socket.on('register', userFirebaseUID => {
-    users[userFirebaseUID] = socket.id;
-  });
-
+  socket.on('register', (userFirebaseUID) => {
+    users[userFirebaseUID] = socket.id
+  })
   socket.on('new-message', async (data) => {
     try {
-      const { message } = data;
+      const { message } = data
 
       await db.message.create({
         text: message.text,
         senderId: message.senderId,
         receiverId: message.receiverId,
-      });
+      })
 
-      const receiverSocketId = users[message.receiverId];
+      const receiverSocketId = users[message.receiverId]
       if (receiverSocketId) {
         io.to(receiverSocketId).emit('new-message', {
           text: message.text,
           senderId: message.senderId,
           receiverId: message.receiverId,
-          time: message.time
-        });
+          time: message.time,
+        })
       }
     } catch (error) {
-      console.error('Error saving message: ', error);
+      console.error('Error saving message: ', error)
     }
-  });
+  })
 
   socket.on('disconnect', () => {
-    Object.keys(users).forEach(uid => {
+    Object.keys(users).forEach((uid) => {
       if (users[uid] === socket.id) {
-        delete users[uid];
+        delete users[uid]
       }
-    });
-  });
-});
+    })
+  })
+})
 
 server.listen(PORT, () => {
-  console.log(`started on port: ${PORT}`);
-});
-
-
-app.use(cors());
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-require("./app/routes/user.routes")(app);
-require("./app/routes/messages.routes")(app);
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to code squad" });
-});
-
-// set port, listen for requests
-
-/* app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-}); */
+  console.log(`started on port: ${PORT}`)
+})
